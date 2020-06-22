289c5d5a87358e0438409025466c9e4cea94ea7d
Johan Villanueva <johan.villanueva@avantica.net>
2020-06-21 22:21:07 -0500

    fix(analytics): env

--- src/analytics.js
+++ src/analytics.js
@@ -2,12 +2,13 @@ export default function initializeAnalytics() {
   try {
     // Set yours custom env variables
     var firebaseConfig = {
-      apiKey: "FIREBASE_API_KEY",
-      projectId: "FIREBASE_APP_NAME",
-      measurementId: "FIREBASE_MEASURE_ID",
-      appId: "FIREBASE_APP_ID",
+      apiKey: $FIREBASE_API_KEY$,
+      projectId: $FIREBASE_APP_NAME$,
+      measurementId: $FIREBASE_MEASURE_ID$,
+      appId: $FIREBASE_APP_ID$,
     };
 
+    console.log(firebaseConfig);
     // Initialize Firebase
     firebaseConfig.apiKey && firebase && firebase.initializeApp(firebaseConfig);
     firebaseConfig.apiKey && firebase && firebase.analytics();
