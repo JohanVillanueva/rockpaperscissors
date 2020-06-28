bb5c862b90f76b35fe595ca1db82a699d57b9fa9
Johan Villanueva <johan.villanueva@avantica.net>
2020-06-27 19:32:10 -0500

    refactor(analytics): reorder imports

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import App from "./App.svelte";
 import initializeAnalytics from "./analytics";
 
-initializeAnalytics();
-
 const app = new App({
-  target: document.body,
+  target: document.getElementById("app"),
 });
+
+initializeAnalytics();
 
 export default app;
